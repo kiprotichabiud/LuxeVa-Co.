f08397e5391e1bb928de572a9a93e4c0c09740ab
Kiprotichabiud <kiprotichabiud1@gmail.com>
2025-04-17 17:01:52 +0300

    Refactor HeroSection component: update paragraph font to enhance readability and consistency

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -81,7 +81,7 @@ const HeroSection = () => {
               <span className="text-pink-700">Business</span>
             </h1>
 
-            <p className="text-base sm:text-lg text-gray-900 font-tertiary font-semibold mb-5 max-w-xl mx-auto lg:mx-0">
+            <p className="text-base font-quaternary sm:text-lg text-gray-900  mb-5 max-w-xl mx-auto lg:mx-0">
               I help ambitious business entrepreneurs reclaim their time through personalized virtual support.
               Let me handle the details while you focus on growing your empire.
             </p>
